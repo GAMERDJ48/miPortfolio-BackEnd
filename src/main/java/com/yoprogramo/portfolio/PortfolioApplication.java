f7aa6e0a8e3bf4a80f09de576089773cca1c70b6
Franco <holacomoestasbienytu2000@gmail.com>
2024-11-06 17:50:28 -0300

    Agrege el env

--- src/main/java/com/yoprogramo/portfolio/PortfolioApplication.java
+++ src/main/java/com/yoprogramo/portfolio/PortfolioApplication.java
@@ -1,4 +1,5 @@
 package com.yoprogramo.portfolio;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import com.yoprogramo.portfolio.entities.*;
 import com.yoprogramo.portfolio.services.*;
@@ -10,7 +11,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PortfolioApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
 
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(PortfolioApplication.class, args);
 
 	}
